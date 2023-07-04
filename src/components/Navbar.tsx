d965940d46aff1a05673426ce9012db39dd81847
khunfloat <60844731+khunfloat@users.noreply.github.com>
2023-07-05 00:19:09 +0700

    feat: make it responsive 😎

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Favicon from '@/public/images/favicon.svg';
 
 function Navbar() {
     return (
-        <nav className="bg-black/50 w-full h-20 flex py-4 justify-between px-10 fixed top-0 left-0 z-50 items-center">
+        <nav className="bg-black/50 w-full lg:h-20 h-11 flex lg:py-4 py-2 justify-between lg:px-10 px-2 fixed top-0 left-0 z-50 items-center">
             <div className="w-12 h-full relative">
                 <Image
                     src={Favicon}
@@ -12,7 +12,7 @@ function Navbar() {
                     className="object-contain"
                 />
             </div>
-            <button className="bg-orange px-6 font-ibm py-2 rounded-2xl text-white text-lg">
+            <button className="bg-orange lg:px-6 px-3 font-ibm lg:py-2 py-1 rounded-2xl text-white lg:text-lg text-xs">
                 เข้าสู่ระบบ
             </button>
         </nav>
