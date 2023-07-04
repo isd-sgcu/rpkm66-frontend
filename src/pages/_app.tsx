c9183b3a1296ff55dcf881ab2b93b80f9a6b5827
shalluv <leon555thai@gmail.com>
2023-07-05 03:59:00 +0700

    feat: background component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Background from '@/components/Background';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { IBM_Plex_Sans_Thai } from 'next/font/google';
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <main className={`${ibmPlexSansThai.variable} font-sans`}>
             <Component {...pageProps} />
+            <Background />
         </main>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,39 @@
 import Image from 'next/image';
 
-import Rocket from '@/public/images/rocket.svg';
 import RocketSmoke from '@/public/images/rocket-smoke.svg';
+import Rocket from '@/public/images/rocket.svg';
+import { shimmer, toBase64 } from '@/utils/shimmer';
 
 const Home = () => {
     return (
-        <div className="w-screen min-h-screen flex flex-col justify-center items-center font-ibm bg-background-mobile lg:bg-background-desktop bg-no-repeat bg-cover bg-center text-white text-center relative z-0 overflow-clip select-none">
-            <h1 className="text-5xl lg:text-9xl font-bold relative z-0">
+        <div className="w-screen min-h-screen flex flex-col justify-center items-center font-ibm text-white text-center relative z-0 overflow-clip">
+            <h1 className="text-5xl lg:text-9xl font-bold relative z-0 leading-none select-none">
                 รับเพื่อนก้าวใหม่
                 <Image
                     src={Rocket}
                     alt=""
-                    className="absolute left-16 -top-16 w-32 h-32 -z-10 lg:-top-32 lg:w-52 lg:h-52 -translate-x-3/4 -translate-y-3/4 rotate-12 lg:rotate-0 origin-bottom"
+                    loading="lazy"
+                    placeholder="blur"
+                    blurDataURL={`data:image/svg+xmlpng;base64, ${toBase64(
+                        shimmer(1024, 1024)
+                    )}`}
+                    className="absolute left-16 -top-16 w-32 h-32 -z-10 lg:-top-32 lg:w-52 lg:h-52 -translate-x-3/4 -translate-y-3/4 rotate-12 lg:rotate-0 origin-bottom select-none pointer-events-none"
                 />
                 <Image
                     src={RocketSmoke}
                     alt=""
-                    className="absolute left-16 -top-16 lg:-top-32 rotate-12 lg:rotate-0 origin-top-left pointer-events-none -z-20"
+                    priority
+                    placeholder="blur"
+                    blurDataURL={`data:image/svg+xmlpng;base64, ${toBase64(
+                        shimmer(1024, 1024)
+                    )}`}
+                    className="absolute left-16 -top-16 lg:-top-32 rotate-12 lg:rotate-0 origin-top-left pointer-events-none -z-20 select-none"
                 />
             </h1>
-            <h2 className="text-4xl lg:text-8xl font-bold z-0">2023</h2>
-            <button className="bg-pink font-bold text-lg px-6 z-0 py-1 mt-3 lg:text-5xl lg:px-20 lg:py-4 lg:mt-16 focus:outline-none ring-8 lg:ring-16 ring-pink/30 rounded-3xl">
+            <h2 className="text-4xl lg:text-8xl font-bold z-0 select-none">
+                2023
+            </h2>
+            <button className="bg-pink font-bold text-lg px-6 z-0 py-1 mt-3 lg:text-5xl lg:px-20 lg:py-4 lg:mt-16 focus:outline-none ring-8 lg:ring-16 ring-pink/30 rounded-3xl select-none">
                 เข้าสู่ระบบด้วย Chula SSO
             </button>
         </div>
