9cffaddc13ad9bbc4ca8ca703fe37db7067e174a
HotPaotatoes <113345557+ChanatpakornDev@users.noreply.github.com>
2023-07-07 10:55:31 +0700

    refactor: minor adjustment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ import Instagram from '@/public/images/instagram.svg';
 const Footer = () => {
     return (
         <footer className="w-full relative lg:fixed bottom-0 left-0">
-            <div className="w-full bg-[#101223] relative m-0 lg:p-4 pt-3 ">
+            <div className="w-full bg-gray-900 relative m-0 lg:p-4 pt-3 ">
                 <div className="flex flex-col lg:flex-row lg:ml-3 justify-center items-center lg:justify-between relative">
                     <div className="flex flex-col lg:flex-row ">
                         <div className=" mt-3 lg:mr-4 lg:px-3 flex justify-center shrink-0">
@@ -66,12 +66,12 @@ const Footer = () => {
                 </div>
             </div>
             <div className="bg-black w-full p-3 flex justify-center items-center">
-                <a
+                <Link
                     className="text-gray-200 font-light text-xs underline underline-offset-2 font-ibm"
                     href="https://gmail.com"
                 >
                     ข้อตกลงและเงื่อนไขการใช้งาน
-                </a>
+                </Link>
             </div>
         </footer>
     );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,42 +8,40 @@ import { shimmer, toBase64 } from '@/utils/shimmer';
 const Home = () => {
     const { login } = useAuth();
     return (
-        <>
-            <div className="w-full min-h-screen flex flex-col justify-center items-center font-ibm text-white text-center relative z-0 overflow-clip">
-                <h1 className="text-5xl lg:text-9xl font-bold relative z-0 leading-none select-none">
-                    รับเพื่อนก้าวใหม่
-                    <Image
-                        src={Rocket}
-                        alt=""
-                        loading="lazy"
-                        placeholder="blur"
-                        blurDataURL={`data:image/svg+xmlpng;base64, ${toBase64(
-                            shimmer(1024, 1024)
-                        )}`}
-                        className="absolute left-16 -top-16 w-32 h-32 -z-10 lg:-top-32 lg:w-52 lg:h-52 -translate-x-3/4 -translate-y-3/4 rotate-12 lg:rotate-0 origin-bottom select-none pointer-events-none"
-                    />
-                    <Image
-                        src={RocketSmoke}
-                        alt=""
-                        priority
-                        placeholder="blur"
-                        blurDataURL={`data:image/svg+xmlpng;base64, ${toBase64(
-                            shimmer(1024, 1024)
-                        )}`}
-                        className="absolute left-16 -top-16 lg:-top-32 rotate-12 lg:rotate-0 origin-top-left pointer-events-none -z-20 select-none"
-                    />
-                </h1>
-                <h2 className="text-4xl lg:text-8xl font-bold z-0 select-none">
-                    2023
-                </h2>
-                <button
-                    className="bg-pink font-bold text-lg px-6 z-0 py-1 mt-3 lg:text-5xl lg:px-20 lg:py-4 lg:mt-16 focus:outline-none ring-8 lg:ring-16 ring-pink/30 rounded-3xl select-none"
-                    onClick={login}
-                >
-                    เข้าสู่ระบบด้วย Chula SSO
-                </button>
-            </div>
-        </>
+        <div className="w-full min-h-screen flex flex-col justify-center items-center font-ibm text-white text-center relative z-0 overflow-clip">
+            <h1 className="text-5xl lg:text-9xl font-bold relative z-0 leading-none select-none">
+                รับเพื่อนก้าวใหม่
+                <Image
+                    src={Rocket}
+                    alt=""
+                    loading="lazy"
+                    placeholder="blur"
+                    blurDataURL={`data:image/svg+xmlpng;base64, ${toBase64(
+                        shimmer(1024, 1024)
+                    )}`}
+                    className="absolute left-16 -top-16 w-32 h-32 -z-10 lg:-top-32 lg:w-52 lg:h-52 -translate-x-3/4 -translate-y-3/4 rotate-12 lg:rotate-0 origin-bottom select-none pointer-events-none"
+                />
+                <Image
+                    src={RocketSmoke}
+                    alt=""
+                    priority
+                    placeholder="blur"
+                    blurDataURL={`data:image/svg+xmlpng;base64, ${toBase64(
+                        shimmer(1024, 1024)
+                    )}`}
+                    className="absolute left-16 -top-16 lg:-top-32 rotate-12 lg:rotate-0 origin-top-left pointer-events-none -z-20 select-none"
+                />
+            </h1>
+            <h2 className="text-4xl lg:text-8xl font-bold z-0 select-none">
+                2023
+            </h2>
+            <button
+                className="bg-pink font-bold text-lg px-6 z-0 py-1 mt-3 lg:text-5xl lg:px-20 lg:py-4 lg:mt-16 focus:outline-none ring-8 lg:ring-16 ring-pink/30 rounded-3xl select-none"
+                onClick={login}
+            >
+                เข้าสู่ระบบด้วย Chula SSO
+            </button>
+        </div>
     );
 };
 
