ee7fd9bdd24ddc441af77e7db7bfdb2f4a854a84
betich <betich365@gmail.com>
2023-07-09 03:00:36 +0700

    chore: add more meta data and banner image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppProps } from 'next/app';
 import { IBM_Plex_Sans_Thai } from 'next/font/google';
 import Script from 'next/script';
 import { Analytics } from '@vercel/analytics/react';
+import Head from 'next/head';
 
 const ibmPlexSansThai = IBM_Plex_Sans_Thai({
     subsets: ['latin'],
@@ -13,16 +14,58 @@ const ibmPlexSansThai = IBM_Plex_Sans_Thai({
     variable: '--font-ibm',
 });
 
+function MetaData() {
+    return (
+        <Head>
+            <title>รับเพื่อนก้าวใหม่ 2023</title>
+            <meta
+                content="พัฒนาโดยฝ่ายพัฒนาระบบสารสนเทศ องค์การบริหารสโมสรนิสิตจุฬาลงกรณ์มหาวิทยาลัย"
+                name="description"
+            />
+            {/* og */}
+            <meta content="รับเพื่อนก้าวใหม่ 2023" property="og:title" />
+            <meta
+                content="พัฒนาโดยฝ่ายพัฒนาระบบสารสนเทศ องค์การบริหารสโมสรนิสิตจุฬาลงกรณ์มหาวิทยาลัย"
+                property="og:description"
+            />
+            <meta content="https://freshmen2023.sgcu.in.th" property="og:url" />
+            <meta
+                content="https://freshmen2023.sgcu.in.th/images/og.png"
+                property="og:image"
+            />
+            <meta content="image/png" property="og:image:type" />
+            <meta content="1200" property="og:image:width" /> {/* 1.91:1 */}
+            <meta content="630" property="og:image:height" />
+            {/* twitter */}
+            <meta content="รับเพื่อนก้าวใหม่ 2023" name="twitter:title" />
+            <meta
+                content="พัฒนาโดยฝ่ายพัฒนาระบบสารสนเทศ องค์การบริหารสโมสรนิสิตจุฬาลงกรณ์มหาวิทยาลัย"
+                name="twitter:description"
+            />
+            <meta
+                content="https://freshmen2023.sgcu.in.th"
+                name="twitter:url"
+            />
+            <meta
+                content="https://freshmen2023.sgcu.in.th/images/og.png"
+                name="twitter:image"
+            />
+            <meta content="image/png" name="twitter:image:type" />
+            <meta content="1200" name="twitter:image:width" /> {/* 1.91:1 */}
+            <meta content="630" name="twitter:image:height" />
+        </Head>
+    );
+}
+
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <AuthProvider>
+            <MetaData />
+
             <main className={`${ibmPlexSansThai.variable} font-ibm text-white`}>
                 <Component {...pageProps} />
                 <Background />
             </main>
-
-            <title>รับเพื่อนก้าวใหม่ 2023</title>
-            <meta content="รับเพื่อนก้าวใหม่ 2023" name="description" />
 
             <Analytics />
             <Script
