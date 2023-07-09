99027fea83f5bc231738fc28f458e0ecc2c6f001
khunfloat <60844731+khunfloat@users.noreply.github.com>
2023-07-09 20:48:17 +0700

    fix: change padding-x for md device

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Favicon from '@/public/images/favicon.svg';
 
 function Navbar() {
     return (
-        <nav className="bg-black/50 w-full lg:h-20 h-11 flex lg:py-4 py-2 justify-between lg:px-10 px-2 fixed top-0 left-0 z-50 items-center">
+        <nav className="bg-black/50 w-full lg:h-20 h-11 flex lg:py-4 py-2 justify-between lg:px-10 md:px-5 px-2 fixed top-0 left-0 z-50 items-center">
             <div className="w-12 h-full relative">
                 <Image
                     src={Favicon}
