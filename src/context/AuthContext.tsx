aeda275f72c280cdf309fffe70367f305c226175
shalluv <leon555thai@gmail.com>
2023-07-10 03:53:53 +0700

    chore: remove console.log()

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -47,13 +47,11 @@ const AuthProvider = ({ children }: { children: ReactNode }) => {
         const token = localStorage.getItem('token');
         if (token) {
             const userProfile = await getUserProfile();
-            console.log('Hey!');
             if (!userProfile) {
                 localStorage.clear();
                 window.location.href = '/';
                 return;
             }
-            console.log(userProfile);
             if (!userProfile.studentID.startsWith('66')) {
                 localStorage.clear();
                 window.location.href = '/only-107';
