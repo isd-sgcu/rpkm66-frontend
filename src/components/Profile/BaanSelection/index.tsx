4dcbde1dfcecfb214ef2a784951722d157735f37
shalluv <leon555thai@gmail.com>
2023-07-15 21:53:57 +0700

    refactor(profile-page): update index

--- src/components/Profile/BaanSelection/index.tsx
+++ src/components/Profile/BaanSelection/index.tsx
@@ -4,10 +4,10 @@ import { StarIcon, XCircleIcon } from '@heroicons/react/24/solid';
 import { Square2StackIcon, CheckCircleIcon } from '@heroicons/react/24/outline';
 import { IShortUser, IUser } from '@/types/user';
 import { IGroup, IGroupPublic } from '@/types/group';
-import InviteLink from './elements/InviteLink';
-import GroupPanel from './elements/GroupPanel';
-import BaanPanel from './elements/BaanPanel';
-import ProfileInfoPanel from './elements/ProfileInfoPanel';
+import InviteLink from './components/InviteLink';
+import GroupPanel from './components/GroupPanel';
+import BaanPanel from './components/BaanPanel';
+import ProfileInfoPanel from './components/ProfileInfoPanel';
 
 const profilePic = '/images/pfp-placeholder.svg';
 
@@ -26,23 +26,18 @@ export interface SelectedBaan {
 
 const BaanSelection = () => {
     return (
-        <>
-            <div className="grid place-items-center lg:mx-5 lg:h-screen ">
-                <div className="my-5 flex flex-col items-center justify-center overflow-clip text-xl text-white lg:my-0 lg:h-full lg:w-full lg:flex-row lg:text-4xl">
-                    <ProfileInfoPanel />
-
-                    <div
-                        className={`bg-style mb-10 w-full grid-rows-2 items-center justify-center rounded-b-2xl bg-black bg-opacity-50 lg:mb-0 lg:mr-5 lg:h-[30rem] lg:w-auto lg:rounded-r-2xl lg:rounded-bl-none`}
-                    >
-                        <InviteLink />
-                        <div className="flex w-full flex-col items-stretch justify-center lg:flex-row">
-                            <GroupPanel />
-                            <BaanPanel />
-                        </div>
+        <div className="flex min-h-screen w-full items-center justify-center overflow-clip py-16 text-white">
+            <div className="flex h-full w-full flex-col items-center justify-center px-4 lg:flex-row">
+                <ProfileInfoPanel />
+                <div className="flex w-full flex-col items-center justify-center rounded-b-2xl bg-black/50 p-8 lg:w-auto lg:rounded-r-2xl lg:rounded-bl-none">
+                    <InviteLink />
+                    <div className="flex w-full flex-col justify-center lg:flex-row">
+                        <GroupPanel />
+                        <BaanPanel />
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
