c868a8363deec7f1c657d9a7be6d4bb935f6c520
Dalai Jotisalikorn Surenjargal <dalai2547@gmail.com>
2023-07-22 01:04:05 +0700

    feat: add stamp

---
+++ src/components/estamp-home/Stamp.tsx
@@ -0,0 +1,22 @@
+import { CheckBadgeIcon } from '@heroicons/react/24/solid';
+import Image from 'next/image';
+import placeHolderPicture from '@/public/images/pfp-placeholder.svg';
+const Stamp = () => {
+    return (
+        <div className="my-8 flex w-4/5 flex-col items-center justify-center text-xl font-bold">
+            <div className="relative aspect-square h-auto w-full max-w-full rounded-xl bg-white ring-4 ring-blue-950">
+                <Image
+                    src={placeHolderPicture}
+                    fill
+                    alt="placeholder-icon"
+                    className="rounded-xl"
+                />
+            </div>
+            <button className="my-4 flex h-12 w-full items-center justify-center rounded-xl bg-yellow ring-4 ring-yellow/40">
+                <CheckBadgeIcon className="mx-2 h-8 w-8" />
+                <h1>Redeem Ticket</h1>
+            </button>
+        </div>
+    );
+};
+export default Stamp;
--- src/pages/estamp-home.tsx
+++ src/pages/estamp-home.tsx
@@ -1,13 +1,15 @@
 import CheckIn from '@/components/estamp-home/CheckIn';
 import MyETicket from '@/components/estamp-home/MyETicket';
 import Profile from '@/components/estamp-home/Profile';
+import Stamp from '@/components/estamp-home/Stamp';
 
 const EstampHome = () => {
     return (
-        <div className="flex min-h-screen w-full flex-col items-center px-6 py-16">
+        <div className="flex min-h-screen w-full flex-col items-center px-6 py-10">
             <Profile />
             <MyETicket />
             <CheckIn />
+            <Stamp />
         </div>
     );
 };
