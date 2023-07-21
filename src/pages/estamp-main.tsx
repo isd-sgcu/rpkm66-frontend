c25ab2cb800e4f277eaa022427154c9a113f94e0
Dalai Jotisalikorn Surenjargal <dalai2547@gmail.com>
2023-07-21 23:11:02 +0700

    feat: estamp main ui skeleton

---
+++ src/pages/estamp-main.tsx
@@ -0,0 +1,9 @@
+const EstampMain = () => {
+    return (
+        <div className="flex min-h-screen w-full justify-center px-6 py-24">
+            <h1>Estamp Main</h1>
+        </div>
+    );
+};
+
+export default EstampMain;
