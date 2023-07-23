a481ea1db0e3aa7c95c574f76b7e330340ec72fb
shalluv <leon555thai@gmail.com>
2023-07-23 13:05:48 +0700

    fix: edit no baan page

--- src/pages/announce-baan/index.tsx
+++ src/pages/announce-baan/index.tsx
@@ -1,13 +1,10 @@
+import { useAuth } from '@/context/AuthContext';
+import { IBaan } from '@/types/baan';
+import { getBaan } from '@/utils/baan';
 import type { NextPage } from 'next';
 import Image from 'next/image';
-import Baanposter from '@/public/images/testAnnounceBaan.png';
-import IGIcon from '@/public/images/instagram.svg';
-import { useAuth } from '@/context/AuthContext';
-import { useEffect, useState } from 'react';
-import { getBaan } from '@/utils/baan';
-import { BaanDTO } from '@/dto/baanDTO';
-import { IBaan } from '@/types/baan';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 const AnnounceBaan: NextPage = () => {
     const { user } = useAuth();
@@ -26,7 +23,7 @@ const AnnounceBaan: NextPage = () => {
     return (
         <div className="relative flex min-h-screen w-full items-center overflow-x-hidden bg-gray-800/30 px-2 backdrop-blur-sm backdrop-filter">
             {baan ? (
-                <div className="mx-auto flex flex-col items-center justify-center space-y-6 rounded-3xl bg-white px-6 py-8 text-black">
+                <div className="mx-auto flex flex-col items-center justify-center space-y-6 rounded-3xl bg-white px-6 py-8 text-purple-400">
                     <div className="w-full text-center text-xl font-semibold text-yellow lg:text-3xl">
                         ยินดีด้วย! คุณได้รับเลือกเข้า
                     </div>
@@ -88,27 +85,14 @@ const AnnounceBaan: NextPage = () => {
                     </div>
                 </div>
             ) : (
-                <div className="mx-auto flex flex-col items-center justify-center space-y-6 rounded-3xl bg-white px-6 py-8 text-black">
-                    <div className="w-full text-center text-xl font-semibold text-yellow lg:text-3xl">
-                        คุณไม่ได้เลือกบ้าน กรุณาเลือกบ้าน
-                    </div>
-                    <div>
-                        <div className="relative h-40 w-40">
-                            <Image
-                                src="/images/Trollface_non-free.png"
-                                alt=""
-                                loading="lazy"
-                                fill
-                                className="rounded-2xl"
-                            />
-                        </div>
-                    </div>
+                <div className="mx-auto flex flex-col items-center justify-center space-y-6 rounded-3xl bg-white px-6 py-8 text-purple-400">
+                    <p className="text-purple w-full text-center text-xl font-semibold lg:text-3xl">
+                        ขออภัย ท่านไม่ได้ทำการเลือกบ้าน
+                        <br /> หรือหากเกิดข้อผิดพลาด
+                    </p>
                     <div className="space-y-2">
-                        <div className="text-center text-4xl font-bold">
-                            ไม่มีบ้านค้าบบบบ
-                        </div>
                         <div className="text-center text-xl font-semibold">
-                            ติดตามข่าวสารได้ที่
+                            กรุณาติดต่อเราที่
                         </div>
                         <div className="flex flex-row items-center justify-center gap-8">
                             <Link
