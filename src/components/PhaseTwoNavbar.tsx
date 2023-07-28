dbf7607f97776130d0167e9b65c15aa2b359b456
PwFaze <feze2013@hotmail.com>
2023-07-28 17:04:43 +0700

    feat: icon colour related to path

--- src/components/PhaseTwoNavbar.tsx
+++ src/components/PhaseTwoNavbar.tsx
@@ -2,9 +2,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { HomeIcon, NewspaperIcon, BellIcon } from '@heroicons/react/24/solid';
 import Favicon from '@/public/images/favicon.svg';
-import { useAuth } from '@/context/AuthContext';
+import { useRouter } from 'next/router';
 
 function PhaseTwoNavbar() {
+    const router = useRouter();
+
     return (
         <nav className="sticky top-0 z-40 flex h-11 w-full justify-between bg-black/50 px-2 py-2 backdrop-blur-sm md:px-5 lg:h-20 lg:px-10 lg:py-4">
             <div className="relative h-full w-12">
@@ -20,15 +22,33 @@ function PhaseTwoNavbar() {
             <div className="relative flex h-full gap-8">
                 {/* wait for link to be added */}
                 <Link href="/">
-                    <NewspaperIcon className="h-full w-12 hover:text-amber-400 " />
+                    <NewspaperIcon
+                        className={`h-full w-12 hover:text-amber-400 ${
+                            router.pathname === '/article-page'
+                                ? 'text-amber-400'
+                                : 'hover:text-amber-400'
+                        }`}
+                    />
                 </Link>
                 {/* wait for link to be added */}
                 <Link href="/">
-                    <BellIcon className="h-full w-12 hover:text-amber-400 " />
+                    <BellIcon
+                        className={`h-full w-12 hover:text-amber-400 ${
+                            router.pathname === '/WAITTOADD'
+                                ? 'text-amber-400'
+                                : 'hover:text-amber-400'
+                        }`}
+                    />
                 </Link>
                 {/* wait for link to be added */}
                 <Link href="/">
-                    <HomeIcon className="h-full w-12 hover:text-amber-400 " />
+                    <HomeIcon
+                        className={`h-full w-12 hover:text-amber-400 ${
+                            router.pathname === '/'
+                                ? 'text-amber-400'
+                                : 'hover:text-amber-400'
+                        }`}
+                    />
                 </Link>
             </div>
         </nav>
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,4 +1,14 @@
 // import WaitForBaanselection from "@/components/Profile/WaitForBaan";
+import PhaseTwoNavbar from '@/components/PhaseTwoNavbar';
 import BaanSelection from '@/components/Profile/BaanSelection';
+
+const Test = () => {
+    return (
+        <>
+            <BaanSelection />;
+            <PhaseTwoNavbar />
+        </>
+    );
+};
 
 export default BaanSelection;
