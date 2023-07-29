88626838ef32276dfbcdfa23b7d37ef175643726
boomchanotai <boom9387@hotmail.com>
2023-07-29 16:28:25 +0700

    refactor: just add type

--- src/components/DefaultButton.tsx
+++ src/components/DefaultButton.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
-const DefaultButton = ({ content, additionalStyle, onClick, disabled }) => {
+const DefaultButton = ({
+    content,
+    additionalStyle,
+    onClick,
+    disabled,
+}: {
+    content: string;
+    additionalStyle?: string;
+    onClick: () => void;
+    disabled?: boolean;
+}) => {
     const buttonClassName =
         `max-w-full w-40 mx-auto items-center py-2 text-white transition-all duration-500 hover:ring-8 disabled:cursor-not-allowed disabled:ring-0 ` +
         additionalStyle;
