40fff818b6c555822ee09ac665230a39d6cd69d0
PwFaze <feze2013@hotmail.com>
2023-07-29 16:57:30 +0700

    style: formatting code

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,6 @@
 import React from 'react';
 
-interface ButtonProps {
-    content: string;
-    additionalStyle?: string;
-    onClick?: () => void;
-    disabled?: boolean;
-}
-
-const Button: React.FC<ButtonProps> = ({
-    content,
-    additionalStyle,
-    onClick,
-    disabled,
-}) => {
+const Button = ({ content, additionalStyle, onClick, disabled }) => {
     return (
         <button
             className={`mx-auto w-40 max-w-full items-center py-2 text-white transition-all duration-500 hover:ring-8 disabled:cursor-not-allowed disabled:ring-0 ${additionalStyle}`}
