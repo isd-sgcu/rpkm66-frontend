6cdfe6ccbc025ecd608cd9224cfa10a703808b8b
PwFaze <feze2013@hotmail.com>
2023-08-01 19:07:17 +0700

    style: add sponsor to ticket

--- src/pages/e-ticket.tsx
+++ src/pages/e-ticket.tsx
@@ -29,57 +29,62 @@ const ETicket = () => {
     };
 
     return (
-        <div className="flex min-h-screen w-full flex-col">
-            <button
-                className="my-6"
-                onClick={() => router.push('/estamp-home')}
-            >
-                <ArrowUturnLeftIcon className="ml-6 h-9 w-9 md:h-12 md:w-12" />
-            </button>
-            <div ref={imageRef} className="flex items-center justify-center">
-                <Image
-                    src="/images/ticket.svg"
-                    width={350}
-                    height={400}
-                    className="absolute -z-10"
-                    alt="ticket"
-                />
-                <div className="flex flex-col items-center">
-                    <p className="mb-64 mt-24 text-8xl">
-                        {user?.firstname || 'Null'}
-                    </p>
-                    <div className="mt-9 flex items-center">
-                        <div className="relative bottom-2 right-12">
-                            <QRCode
-                                value="https://qr-service.vercel.app/api/code?text=https://freshmen2023.sgcu.in.th/&size=300"
-                                size={127}
-                                fgColor="#3C415F"
-                            />
-                        </div>
-                        <div className="relative left-3 top-2 flex flex-col gap-1 text-center text-blue-950">
-                            <p className="text-4xl">
-                                {user?.firstname || 'Test'}
-                            </p>
-                            <p>{user?.firstname || 'Test'}</p>
+        <>
+            <div className="flex min-h-screen w-full flex-col">
+                <button
+                    className="my-6"
+                    onClick={() => router.push('/estamp-home')}
+                >
+                    <ArrowUturnLeftIcon className="ml-6 h-9 w-9 md:h-12 md:w-12" />
+                </button>
+                <div
+                    ref={imageRef}
+                    className="flex items-center justify-center"
+                >
+                    <Image
+                        src="/images/ticket.svg"
+                        width={350}
+                        height={400}
+                        className="absolute -z-10"
+                        alt="ticket"
+                    />
+                    <div className="flex flex-col items-center">
+                        <p className="mb-64 mt-24 text-8xl">
+                            {user?.firstname || 'Null'}
+                        </p>
+                        <div className="mt-9 flex items-center">
+                            <div className="relative bottom-2 right-12">
+                                <QRCode
+                                    value="https://qr-service.vercel.app/api/code?text=https://freshmen2023.sgcu.in.th/&size=300"
+                                    size={127}
+                                    fgColor="#3C415F"
+                                />
+                            </div>
+                            <div className="relative left-3 top-2 flex flex-col gap-1 text-center text-blue-950">
+                                <p className="text-4xl">
+                                    {user?.firstname || 'Test'}
+                                </p>
+                                <p>{user?.firstname || 'Test'}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <Button
+                    content={
+                        <div className="flex items-center justify-center">
+                            <ArrowDownTrayIcon
+                                width={30}
+                                height={30}
+                                className="mx-2"
+                            />
+                            <p className="font-bold">Save Image</p>
+                        </div>
+                    }
+                    onClick={saveImage}
+                    additionalStyle="mt-10 mb-20 py-2 w-80 rounded-lg bg-pink-400 ring-pink-400/30 ring-4"
+                />
             </div>
-            <Button
-                content={
-                    <div className="flex items-center justify-center">
-                        <ArrowDownTrayIcon
-                            width={30}
-                            height={30}
-                            className="mx-2"
-                        />
-                        <p className="font-bold">Save Image</p>
-                    </div>
-                }
-                onClick={saveImage}
-                additionalStyle="mt-10 mb-20 py-2 w-80 rounded-lg bg-pink-400 ring-pink-400/30 ring-4"
-            />
-        </div>
+        </>
     );
 };
 
