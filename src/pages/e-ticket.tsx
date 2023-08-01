7ed8c82f68292475f10357ad360e998d32d8ffdf
Dalai Jotisalikorn Surenjargal <dalai2547@gmail.com>
2023-08-01 18:45:51 +0700

    fix: qr code scan

--- src/pages/e-ticket.tsx
+++ src/pages/e-ticket.tsx
@@ -1,33 +1,84 @@
 import QRCode from 'react-qr-code';
-import Link from 'next/link';
+import { useAuth } from '@/context/AuthContext';
 import Image from 'next/image';
-
-// using https://www.npmjs.com/package/react-qr-code
-
-const qrcode = 'mockexampletest';
+import {
+    ArrowUturnLeftIcon,
+    ArrowDownTrayIcon,
+} from '@heroicons/react/24/solid';
+import Button from '@/components/Button';
+import { useRouter } from 'next/router';
+import { useRef } from 'react';
+import { toPng } from 'html-to-image';
 
 const ETicket = () => {
+    const { user } = useAuth();
+    const router = useRouter();
+    const imageRef = useRef<HTMLDivElement>(null);
+    const saveImage = () => {
+        toPng(imageRef.current as HTMLElement, { quality: 0.95 }).then(
+            (dataUrl: string) => {
+                const link = document.createElement('a');
+                link.download = `${user?.firstname || '__'}-${
+                    user?.lastname || '__'
+                }-e-ticket.png`;
+                link.href = dataUrl;
+                link.click();
+                link.remove();
+            }
+        );
+    };
+
     return (
-        <div className="min-h-screen flex-col">
-            <Link href="https://freshmen2023.sgcu.in.th/profile">
-                <button className="relative flex h-20 w-20 px-4 py-1">
-                    <Image
-                        //change icon to arrow-uturn-left in heroicons.com
-                        src="/images/icons8-instagram.svg"
-                        alt="Go back"
-                        fill
-                    />
-                </button>
-            </Link>
-            <div className="m-auto flex max-w-fit rounded-3xl bg-white p-6">
-                <div className="max-h-fit">
-                    <QRCode
-                        value={qrcode}
-                        //mote: change fgColor
-                        fgColor="#3C415F"
-                    />
+        <div className="flex min-h-screen w-full flex-col">
+            <button
+                className="my-6"
+                onClick={() => router.push('/estamp-home')}
+            >
+                <ArrowUturnLeftIcon className="ml-6 h-9 w-9 md:h-12 md:w-12" />
+            </button>
+            <div ref={imageRef} className="flex items-center justify-center">
+                <Image
+                    src="/images/ticket.svg"
+                    width={350}
+                    height={400}
+                    className="absolute -z-10"
+                    alt="ticket"
+                />
+                <div className="flex flex-col items-center">
+                    <p className="mb-64 mt-24 text-8xl">
+                        {user?.firstname || 'Null'}
+                    </p>
+                    <div className="mt-9 flex items-center">
+                        <div className="relative bottom-2 right-12">
+                            <QRCode
+                                value="https://qr-service.vercel.app/api/code?text=https://freshmen2023.sgcu.in.th/&size=300"
+                                size={127}
+                                fgColor="#3C415F"
+                            />
+                        </div>
+                        <div className="relative left-3 top-2 flex flex-col gap-1 text-center text-blue-950">
+                            <p className="text-4xl">
+                                {user?.firstname || 'Test'}
+                            </p>
+                            <p>{user?.firstname || 'Test'}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <Button
+                content={
+                    <div className="flex items-center justify-center">
+                        <ArrowDownTrayIcon
+                            width={30}
+                            height={30}
+                            className="mx-2"
+                        />
+                        <p className="font-bold">Save Image</p>
+                    </div>
+                }
+                onClick={saveImage}
+                additionalStyle="mt-10 mb-20 py-2 w-80 rounded-lg bg-pink-400 ring-pink-400/30 ring-4"
+            />
         </div>
     );
 };
