a8d157c6f2435b605361840eaa1f2e50e8abab16
Dalai Jotisalikorn Surenjargal <dalai2547@gmail.com>
2023-08-01 17:13:17 +0700

    refactor: add another div

--- src/pages/e-ticket.tsx
+++ src/pages/e-ticket.tsx
@@ -12,25 +12,22 @@ const ETicket = () => {
     const router = useRouter();
 
     return (
-        <>
-            <Button
-                content={
-                    <>
-                        <ArrowUturnLeftIcon className="ml-6 h-12 w-12" />
-                    </>
-                }
+        <div className="flex min-h-screen w-full flex-col">
+            <button
+                className="my-6"
                 onClick={() => router.push('/estamp-home')}
-            />
-
-            <div className="flex h-auto min-h-screen flex-col items-center justify-items-center">
+            >
+                <ArrowUturnLeftIcon className="ml-6 h-9 w-9 md:h-12 md:w-12" />
+            </button>
+            <div className="flex items-center justify-center">
                 <Image
                     src="/images/ticket.svg"
                     width={350}
                     height={400}
-                    className="absolute"
+                    className="absolute -z-10"
                     alt="ticket"
                 />
-                <div className="relative flex flex-col items-center">
+                <div className="flex flex-col items-center">
                     <p className="mb-64 mt-24 text-8xl">
                         {user?.firstname || 'Null'}
                     </p>
@@ -43,33 +40,33 @@ const ETicket = () => {
                             height={127}
                             src="https://qr-service.vercel.app/api/code?text=https://freshmen2023.sgcu.in.th/&size=300"
                         />
-                        <div className="relative left-3 top-4 flex flex-col gap-1 text-center text-blue-950">
+                        <div className="relative left-3 top-2 flex flex-col gap-1 text-center text-blue-950">
                             <p className="text-4xl">
                                 {user?.firstname || 'Null'}
                             </p>
                             <p>{user?.firstname || 'Null'}</p>
                         </div>
                     </div>
-                    <Button
-                        content={
-                            <div className="flex items-center justify-center">
-                                <ArrowDownTrayIcon
-                                    width={30}
-                                    height={30}
-                                    className="mx-2"
-                                />
-                                <p className="font-bold">Save Image</p>
-                            </div>
-                        }
-                        // wait for saveImage method
-                        onClick={() => {
-                            router.push('/');
-                        }}
-                        additionalStyle="mt-10 mb-20 py-2 w-full rounded-lg bg-pink-400 ring-pink-400/30 ring-4"
-                    />
                 </div>
             </div>
-        </>
+            <Button
+                content={
+                    <div className="flex items-center justify-center">
+                        <ArrowDownTrayIcon
+                            width={30}
+                            height={30}
+                            className="mx-2"
+                        />
+                        <p className="font-bold">Save Image</p>
+                    </div>
+                }
+                // wait for saveImage method
+                onClick={() => {
+                    router.push('/');
+                }}
+                additionalStyle="mt-10 mb-20 py-2 w-80 rounded-lg bg-pink-400 ring-pink-400/30 ring-4"
+            />
+        </div>
     );
 };
 
