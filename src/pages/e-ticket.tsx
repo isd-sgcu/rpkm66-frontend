ee6a37ce8db988bde502e11b7e0ac4a30886bd9b
Dalai Jotisalikorn Surenjargal <dalai2547@gmail.com>
2023-08-01 15:14:33 +0700

    fix: design in iphone

--- src/pages/e-ticket.tsx
+++ src/pages/e-ticket.tsx
@@ -1,18 +1,12 @@
 import { useAuth } from '@/context/AuthContext';
 import Image from 'next/image';
-import Link from 'next/link';
 import {
     ArrowUturnLeftIcon,
     ArrowDownTrayIcon,
 } from '@heroicons/react/24/solid';
-import Sponsor from '@/components/Sponsor';
 import Ticket from '@/public/images/ticket.svg';
 import Button from '@/components/Button';
 import { useRouter } from 'next/router';
-import { useRef } from 'react';
-
-// using https://github.com/dephraiim/qrcode
-// example: https://qr-service.vercel.app/api/code?text=https://freshmen2023.sgcu.in.th/&size=200
 
 const ETicket = () => {
     const { user } = useAuth();
@@ -20,20 +14,26 @@ const ETicket = () => {
 
     return (
         <>
-            <Link href="https://freshmen2023.sgcu.in.th/profile">
-                <ArrowUturnLeftIcon className="w-2- ml-6 h-20" />
-            </Link>
+            <Button
+                content={
+                    <>
+                        <ArrowUturnLeftIcon className="ml-6 h-12 w-12" />
+                    </>
+                }
+                onClick={() => router.push('/estamp-home')}
+            />
+
             <div className="flex h-auto min-h-screen flex-col items-center justify-items-center">
                 <Image
                     src={Ticket}
+                    width={350}
+                    height={400}
                     className="absolute"
-                    width={380}
-                    height={335}
                     alt="ticket"
                 />
-                <div className="relative top-10 flex min-h-screen flex-col items-center">
-                    <p className="mb-72 mt-24 text-7xl">
-                        {user?.firstname || 'เฟซ'}
+                <div className="relative flex flex-col items-center">
+                    <p className="mb-64 mt-24 text-8xl">
+                        {user?.firstname || 'Null'}
                     </p>
                     <div className="mt-9 flex items-center">
                         <Image
@@ -44,7 +44,7 @@ const ETicket = () => {
                             height={127}
                             src="https://qr-service.vercel.app/api/code?text=https://freshmen2023.sgcu.in.th/&size=300"
                         />
-                        <div className="relative left-5 top-4 flex flex-col gap-1 text-center text-blue-950">
+                        <div className="relative left-3 top-4 flex flex-col gap-1 text-center text-blue-950">
                             <p className="text-4xl">
                                 {user?.firstname || 'Null'}
                             </p>
@@ -53,21 +53,20 @@ const ETicket = () => {
                     </div>
                     <Button
                         content={
-                            <>
+                            <div className="flex items-center justify-center">
                                 <ArrowDownTrayIcon
-                                    width={40}
-                                    height={40}
-                                    className="inline-block"
+                                    width={30}
+                                    height={30}
+                                    className="mx-2"
                                 />
-                                <p>Save Image</p>
-                            </>
+                                <p className="font-bold">Save Image</p>
+                            </div>
                         }
                         // wait for saveImage method
                         onClick={() => {
                             router.push('/');
                         }}
-                        additionalStyle="gap-4 mt-10 mb-20 py-2 flex w-full px-32 rounded-lg bg-pink-400 ring-pink-400/30"
-                        disabled={false}
+                        additionalStyle="mt-10 mb-20 py-2 w-full rounded-lg bg-pink-400 ring-pink-400/30 ring-4"
                     />
                 </div>
             </div>
