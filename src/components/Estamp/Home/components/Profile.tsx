852031219a0b71f1e2959ad96774a4042d58122c
boomchanotai <boom9387@hotmail.com>
2023-08-03 02:29:36 +0700

    refactor: styling

--- src/components/Estamp/Home/components/Profile.tsx
+++ src/components/Estamp/Home/components/Profile.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { getBaan } from '@/utils/baan';
 import { IUser } from '@/types/user';
+import Container from './Container';
 const Profile = () => {
     const { user } = useAuth();
     const router = useRouter();
@@ -20,37 +21,41 @@ const Profile = () => {
     }, [user]);
 
     return (
-        <div className="flex w-full justify-center gap-x-8 rounded-3xl bg-white p-8 ring-4 ring-white/40">
-            <div className="flex items-center justify-center">
-                <div className="relative aspect-square h-36 w-24 max-w-full rounded-xl ring-4 ring-blue-950">
-                    <Image
-                        src={user?.imageUrl || placeHolder}
-                        fill
-                        alt="Photo"
-                        className="relative rounded-xl object-cover"
-                    />
-                </div>
-            </div>
-            <div className="w-full space-y-4 text-lg font-bold text-purple-400">
-                <div>
-                    <h1>{user?.firstname || 'Null'}</h1>
-                    <h1>{user?.lastname || 'Null'}</h1>
-                </div>
-                <div className="flex w-full justify-center rounded-xl border-2 border-pink-400 px-4 py-1 text-pink-400">
-                    <HomeIcon className="h-4 w-4" />
-                    <p className="mx-1 text-sm">{baanName || 'ไม่มีบ้าน'}</p>
-                </div>
-                <button
-                    className="w-full rounded-md bg-purple-400 py-1 ring-4 ring-gray-300"
-                    onClick={() => router.push('/edit')}
-                >
-                    <div className="flex justify-center py-1 text-sm text-white">
-                        <p>แก้ไขข้อมูล</p>
-                        <PencilSquareIcon className="ml-1 h-5 w-4" />
+        <Container>
+            <div className="flex w-full justify-center gap-x-8 rounded-3xl bg-white p-8 ring-4 ring-white/40">
+                <div className="flex items-center justify-center">
+                    <div className="relative aspect-square h-36 w-24 max-w-full rounded-xl ring-4 ring-blue-950">
+                        <Image
+                            src={user?.imageUrl || placeHolder}
+                            fill
+                            alt="Photo"
+                            className="relative rounded-xl object-cover"
+                        />
                     </div>
-                </button>
+                </div>
+                <div className="w-full space-y-4 text-lg font-bold text-purple-400">
+                    <div>
+                        <h1>{user?.firstname || 'Null'}</h1>
+                        <h1>{user?.lastname || 'Null'}</h1>
+                    </div>
+                    <div className="flex w-full justify-center rounded-xl border-2 border-pink-400 px-4 py-1 text-pink-400">
+                        <HomeIcon className="h-4 w-4" />
+                        <p className="mx-1 text-sm">
+                            {baanName || 'ไม่มีบ้าน'}
+                        </p>
+                    </div>
+                    <button
+                        className="w-full rounded-md bg-purple-400 py-1 ring-4 ring-gray-300"
+                        onClick={() => router.push('/edit')}
+                    >
+                        <div className="flex justify-center py-1 text-sm text-white">
+                            <p>แก้ไขข้อมูล</p>
+                            <PencilSquareIcon className="ml-1 h-5 w-4" />
+                        </div>
+                    </button>
+                </div>
             </div>
-        </div>
+        </Container>
     );
 };
 
--- src/components/Estamp/Home/components/Stamp.tsx
+++ src/components/Estamp/Home/components/Stamp.tsx
@@ -8,6 +8,7 @@ import { useAuth } from '@/context/AuthContext';
 import { useToast } from '@/components/Toast';
 import { httpPost } from '@/utils/axios';
 import { getRedeemStatus, getUserStamp } from '@/api/stamp';
+import Container from './Container';
 
 const Stamp = () => {
     const { isAuthenticated, isReady } = useAuth();
@@ -46,52 +47,58 @@ const Stamp = () => {
     }, [isRedeemed]);
 
     return (
-        <div className="my-3 flex w-4/5 flex-col items-center justify-center text-xl font-bold md:w-1/2">
-            <div className="relative flex aspect-square h-auto w-full max-w-full items-center justify-center">
-                {isRedeemed && (
-                    <h1 className="absolute z-20 text-4xl text-white md:text-6xl">
-                        REDEEMED
-                    </h1>
-                )}
-                <Image
-                    src={'/images/estamp-background.png'}
-                    alt="background"
-                    fill
-                    className="absolute -z-10"
-                />
-                <div className="flex-block grid h-full w-full grid-cols-2 gap-1 p-3">
-                    {stampPiecePicture.map(
-                        (stampPiece: EstampEvent, index: number) => {
-                            return (
-                                <StampPiece
-                                    key={stampPiece.id}
-                                    id={stampPiece.id}
-                                    style={stampPieceStyle[index].style}
-                                    imgUrl={stampPieceStyle[index].imgUrl}
-                                    isVisible={index < (userStamp?.length || 0)}
-                                />
-                            );
-                        }
+        <Container className="flex flex-col items-center justify-center">
+            <div className="w-full md:w-1/2">
+                <div className="relative flex aspect-square h-auto w-full max-w-full items-center justify-center">
+                    {isRedeemed && (
+                        <h1 className="absolute z-20 text-4xl text-white md:text-4xl">
+                            REDEEMED
+                        </h1>
                     )}
+                    <Image
+                        src={'/images/estamp-background.png'}
+                        alt="background"
+                        fill
+                        className="absolute -z-10"
+                    />
+                    <div className="flex-block grid h-full w-full grid-cols-2 gap-1 p-3">
+                        {stampPiecePicture.map(
+                            (stampPiece: EstampEvent, index: number) => {
+                                return (
+                                    <StampPiece
+                                        key={stampPiece.id}
+                                        id={stampPiece.id}
+                                        style={stampPieceStyle[index].style}
+                                        imgUrl={stampPieceStyle[index].imgUrl}
+                                        isVisible={
+                                            index < (userStamp?.length || 0)
+                                        }
+                                    />
+                                );
+                            }
+                        )}
+                    </div>
                 </div>
-            </div>
-            <button
-                className={`my-8 flex h-12 w-full items-center justify-center rounded-xl bg-yellow ring-4 ring-yellow/40 transition-all duration-300 ease-in-out hover:ring-8 disabled:opacity-80 disabled:hover:ring-4`}
-                onClick={async () => {
-                    const { status } = await httpPost('/estamp/redeem', {});
-                    if (status === 200) {
-                        toast?.setToast('success', 'Redeem Success');
-                        window.location.reload();
+                <button
+                    className={`my-8 flex h-12 w-full items-center justify-center rounded-xl bg-yellow ring-4 ring-yellow/40 transition-all duration-300 ease-in-out hover:ring-8 disabled:opacity-80 disabled:hover:ring-4`}
+                    onClick={async () => {
+                        const { status } = await httpPost('/estamp/redeem', {});
+                        if (status === 200) {
+                            toast?.setToast('success', 'Redeem Success');
+                            window.location.reload();
+                        }
+                    }}
+                    disabled={
+                        (userStamp?.length ?? 0) < 4 || isRedeemed
+                            ? true
+                            : false
                     }
-                }}
-                disabled={
-                    (userStamp?.length ?? 0) < 4 || isRedeemed ? true : false
-                }
-            >
-                <CheckBadgeIcon className="mx-2 h-8 w-8" />
-                <h1>Redeem Ticket</h1>
-            </button>
-        </div>
+                >
+                    <CheckBadgeIcon className="mx-2 h-8 w-8" />
+                    <h1>Redeem Ticket</h1>
+                </button>
+            </div>
+        </Container>
     );
 };
 export default Stamp;
