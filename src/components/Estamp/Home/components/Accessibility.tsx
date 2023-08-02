aa750023ad5968310244bd0fb6def0826bad9303
boomchanotai <boom9387@hotmail.com>
2023-08-03 02:50:01 +0700

    refactor: button component

--- src/components/Estamp/Home/components/Accessibility.tsx
+++ src/components/Estamp/Home/components/Accessibility.tsx
@@ -1,25 +1,27 @@
 import { CameraIcon, QrCodeIcon } from '@heroicons/react/24/solid';
-import { useRouter } from 'next/router';
 import Container from './Container';
+import Link from 'next/link';
+import Button from './Button';
 
 const Accessibility = () => {
-    const router = useRouter();
     return (
-        <Container>
-            <button
-                className="mt-6 flex w-full items-center justify-center rounded-xl bg-pink-400 py-2 text-xl font-bold ring-4 ring-pink-400/40 transition-all duration-300 ease-in-out hover:ring-8"
-                onClick={() => router.push('/e-ticket')}
-            >
-                <QrCodeIcon className="mx-2 h-8 w-8" />
-                <h1>My E-Ticket</h1>
-            </button>
-            <button
-                className="my-6 flex w-full items-center justify-center rounded-xl bg-green py-2 text-xl font-bold ring-4 ring-green/40 transition-all duration-300 ease-in-out hover:ring-8"
-                onClick={() => router.push('/scan')}
-            >
-                <CameraIcon className="mx-2 h-6 w-6" />
-                <h1>Check-in / Check-out</h1>
-            </button>
+        <Container className="my-6 space-y-6">
+            <div>
+                <Link href="/e-ticket">
+                    <Button color="pink">
+                        <QrCodeIcon className="mx-2 h-8 w-8" />
+                        <h1>My E-Ticket</h1>
+                    </Button>
+                </Link>
+            </div>
+            <div>
+                <Link href="/scan">
+                    <Button color="green">
+                        <CameraIcon className="mx-2 h-6 w-6" />
+                        <h1>Check-in / Check-out</h1>
+                    </Button>
+                </Link>
+            </div>
         </Container>
     );
 };
